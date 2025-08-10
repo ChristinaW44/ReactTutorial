26f84b04fca234d2d875ec6691b728918f8b8c2b
ChristinaW44 <103203583+ChristinaW44@users.noreply.github.com>
2025-08-10 21:39:41 +0100

    added children

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,18 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["1", "2", "3", "4"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Numbers"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span> World</span>
+      </Alert>
     </div>
   );
 }
