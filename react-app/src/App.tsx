85f47eacb10c64c3724ee320e694e8fb7cba1d08
ChristinaW44 <103203583+ChristinaW44@users.noreply.github.com>
2025-08-10 21:29:44 +0100

    added prop function

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,10 +2,17 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["1", "2", "3", "4"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
 
   return (
     <div>
-      <ListGroup items={items} heading="Numbers" />
+      <ListGroup
+        items={items}
+        heading="Numbers"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
